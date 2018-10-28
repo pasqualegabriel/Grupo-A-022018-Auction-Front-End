0153cfb8ead452d7867a209bb0f8c4ecf999daf4
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-10-28 19:12:45 -0300

    added scroll to auction list

--- src/components/Auction.js
+++ src/components/Auction.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import {setItem} from '../services/LocalStorageService'
-import { Image, List, Card } from 'semantic-ui-react'
+import { Image, Card } from 'semantic-ui-react'
 
 const blue = {
     color:'#140e63',
@@ -9,21 +9,28 @@ const blue = {
 }
 
 const st = {
-    textAlign       : 'center',
-    border          : '1px solid #e6e6e6',
+    // textAlign       : 'center',
+    // border          : '1px solid #e6e6e6',
     backgroundColor : '#e6e6e6',
-    width: '158px'
+    width: '158px',
+    // padding: '2 50px',
+    margin: '10px'
 }
 
 const st2 = {
     height: '122px'
 }
 
+const st3 = {
+    width: '12px'
+    // height: '122px'
+}
+
 export default ({auction}) => {
 
     return (
 
-      <List.Item>
+    //   <List.Item>
         <Card style={st}>
             <Image 
                 src='https://www.crystalcommerce.com/wp-content/uploads/2018/09/square_gow4-notxt.jpg' 
@@ -32,7 +39,7 @@ export default ({auction}) => {
             <Card.Content>
             <Card.Header>{auction.title}</Card.Header>
             <Card.Meta>$ {auction.price}</Card.Meta>
-            <Card.Description>{auction.description}</Card.Description>
+            <Card.Description style={st3}>{auction.description}</Card.Description>
             </Card.Content>
             <Card.Content extra>
                 <Link style={blue} to={'/detail'} onClick={()=>setItem('auction', auction)}>
@@ -40,7 +47,7 @@ export default ({auction}) => {
                 </Link> 
             </Card.Content>
         </Card>
-      </List.Item>
+    //   </List.Item>
     )
 }
 
--- src/components/AuctionsList.js
+++ src/components/AuctionsList.js
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import './App.css';
 import Auction from './Auction.js'
-import { List } from 'semantic-ui-react'
-
-const styles = {
-    margin: 'auto',
-    marginLeft: '40px',
-    padding: '5em', 
-    width: '100%'
-}
-
-export default ({auctions}) => {
-
-    return ( 
-        <List horizontal relaxed style={styles}>
-                  {auctions.map(anAuction => 
-            <Auction key={anAuction.id} auction={anAuction}/>
-        )}
-        </List>
-
-
-    )
+ 
+// All items component
+// Important! add unique key
+const Menu = (auctions) => 
+                auctions.map(anAuction => {
+                   return ( <Auction key={anAuction.id} auction={anAuction}/> )
+                })
+ 
+const Arrow = ({ text, className }) => {
+  return (
+    <div
+      className={className}
+    >{text}</div>
+  );
+};
+ 
+const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
+const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
+ 
+export default class AuctionList extends Component {
+  state = {
+    selected: 0
+  };
+  
+  onSelect = key => {
+    this.setState({ selected: key });
+  }
+ 
+  render() {
+    const { selected } = this.state;
+    // Create menu from items
+    const menu = Menu(this.props.auctions);
+ 
+    return (
+      <div className="App">
+        <ScrollMenu
+          data={menu}
+          arrowLeft={ArrowLeft}
+          arrowRight={ArrowRight}
+          selected={selected}
+          onSelect={this.onSelect}
+        />
+      </div>
+    );
+  }
 }
