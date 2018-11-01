7a9c9a8ef44e24023c72583e1bf066827bb345e7
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-10-31 23:56:07 -0300

    vertical sidebar updated

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,93 +9,57 @@ import AuctionsSearch from '../components/AuctionsSearch';
 import Auctions from '../components/Auctions';
 import 'react-notifications/lib/notifications.css'
 import Login from './Login'
-import PropTypes from 'prop-types'
 import Home from './Home'
-import {
-  Icon,
-  Menu,
-  Segment,
-  Sidebar,
-} from 'semantic-ui-react'
-
-const VerticalSidebar = ({ animation, direction, visible }) => (
-  <Sidebar
-    as={Menu}
-    animation={animation}
-    direction={direction}
-    icon='labeled'
-    vertical
-    visible={visible}
-    width='thin'
-    heigth='100%'
-  >
-    <Menu.Item as='a'>
-      <Icon name='home' />
-      Home
-    </Menu.Item>
-    <Menu.Item as='a'>
-      <Icon name='gamepad' />
-      Games
-    </Menu.Item>
-    <Menu.Item as='a'>
-      <Icon name='camera' />
-      Channels
-    </Menu.Item>
-  </Sidebar>
-)
-
-VerticalSidebar.propTypes = {
-  animation: PropTypes.string,
-  direction: PropTypes.string,
-  visible: PropTypes.bool,
-}
+import { Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 class App extends Component {
 
-  state = {
-    animation: 'overlay',
-    direction: 'left',
-    dimmed: false,
-    visible: false,
-  }
+  state = { visible: false }
 
-  handleAnimationChange = animation => () =>
-    this.setState({ animation, visible: !this.state.visible })
+  handleShowClick = () => this.setState({ visible: true })
 
-  handleDimmedChange = (e, { checked }) => this.setState({ dimmed: checked })
-
-  handleDirectionChange = direction => () => this.setState({ direction, visible: false })
+  handleSidebarHide = () => this.setState({ visible: false })
 
   getTranslation = (key) => this.props.t(key)
 
-  changeLanguage = (lng) => {
-    this.props.i18n.changeLanguage(lng)
-  }
+  changeLanguage = (lng) => this.props.i18n.changeLanguage(lng)
 
   getLanguage = () => this.props.lng
 
   render() {
 
-    const { animation, direction, dimmed, visible } = this.state
-    const vertical = direction === 'bottom' || direction === 'top'
+    const { visible } = this.state
 
     return (
-      <div>
-        <HeaderM click1={this.handleAnimationChange('push')} 
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={visible}>
+          <HeaderM click1={this.handleShowClick} 
                 getTranslation={this.getTranslation} 
                 changeLanguage={this.changeLanguage}
                 getLanguage={this.getLanguage}/> 
-
-
-        <div>
-          {/* <img src='https://i.ytimg.com/vi/UcMMA7KKBsk/maxresdefault.jpg' alt="" sizes></img> */}
-        </div>
-        <Sidebar.Pushable as={Segment}>
-          {vertical ? null : (
-            <VerticalSidebar animation={animation} direction={direction} visible={visible} />
-          )}
-
-          <Sidebar.Pusher dimmed={dimmed && visible}>
             <Switch>
               <Route exact path="/home"     render={()=><Home/>}/>
               <Route exact path="/signIn"   render={()=><Login getTranslation={this.getTranslation} />}/>
@@ -106,9 +70,7 @@ class App extends Component {
             </Switch>
           </Sidebar.Pusher>
         </Sidebar.Pushable>
-        
-      </div>
-    );
+    )
   }
 }
 
