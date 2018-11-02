1cd419f110bd03dca49504d0d7507014ec963ecf
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-11-02 17:22:39 -0300

    auction details updated

--- src/components/AuctionDetails.js
+++ src/components/AuctionDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {getItem} from '../services/LocalStorageService'
-import { Table } from 'semantic-ui-react'
+import { Table, Button, Label } from 'semantic-ui-react'
 import 'moment/locale/es'
 import moment from 'moment'
 
@@ -54,24 +54,6 @@ export default class App extends Component {
     })
   }
 
-  /*
-address: null
-automaticOfferAmount: 200
-bidders: (3) [{…}, {…}, {…}]
-currentState: "COMPLETED"
-description: "The goddess Athena tasks Kratos with killing Ares."
-emailAuthor: "user@gmail.com"
-finishDate: "2018-09-28T14:13:30"
-finished: true
-id: 76
-inProgress: false
-initialFinishDate: "2018-09-28T14:13:30"
-photos: null
-price: 100
-publicationDate: "2018-09-25T14:13:30"
-title: "PS4 God of War"
-  */
-
   render() {
 
     return (
@@ -85,6 +67,33 @@ title: "PS4 God of War"
           </div>
         </div>
         <div style={middlepane}>
+
+          <Table celled textAlign='center' >
+
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Ultimo tramo</Table.HeaderCell>
+                <Table.HeaderCell>
+                <Label color='teal'>
+                  <h2>$ {this.state.auction.price}</h2>
+                </Label>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Tramo 10 - $ 100</Table.Cell>
+                <Table.Cell>
+                  <Button primary>
+                    <h3>Realizar offerta</h3>
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+
+          </Table>
+
           <Table celled textAlign='center' >
             <Table.Header>
               <Table.Row>
@@ -115,82 +124,25 @@ title: "PS4 God of War"
 }
 
 /*
-
+--auction--
+automaticOfferAmount: 200
+bidders: (3) [{…}, {…}, {…}]
+currentState: "COMPLETED"
+description: "The goddess Athena tasks Kratos with killing Ares."
+emailAuthor: "user@gmail.com"
+finishDate: "2018-09-28T14:13:30"
+finished: true
+id: 76
+inProgress: false
+initialFinishDate: "2018-09-28T14:13:30"
+photos: null
+price: 100
+publicationDate: "2018-09-25T14:13:30"
+title: "PS4 God of War"
+--offer--
 auction: null
 author: "user2@gmail.com"
 id: 77
 price: 200
 publicationDate: "2018-09-28T14:13:30
-
-      <div>
-          {this.state.auction.emailAuthor}
-          {this.state.bidders.map(b => 
-            <div key={b.id}>
-              <h1>{b.author}</h1>
-            </div>
-          )}
-      </div>
-
-      <Grid container columns={2}>
-        
-        <Grid.Column>
-          <div>
-              <div className='login-form'>
-              
-                Heads up! The styles below are necessary for the correct render of this example.
-                You can do same with CSS, the main idea is that all the elements up to the `Grid`
-                below must have a height of 100%.
-              
-              <style>{`
-                body > div,
-                body > div > div,
-                body > div > div > div.login-form {
-                  height: 100%;
-                }
-              `}</style>
-              <Grid textAlign='center' style={styles.loginStyle} verticalAlign='middle' >
-                <Grid.Column style={{ maxWidth: 500 }}>
-                  
-                  <Form size='large' >
-                    <Segment stacked>
-                    <Header as='h2' color='teal' textAlign='center'>
-                     <Image src="../UNQ Black Logo.png" />  
-                    Log-in to your account
-                  </Header>
-                      <Form.Input name="name" fluid icon='user' 
-                                  iconPosition='left' placeholder='Username' />
-                      <Form.Input
-                        fluid
-                        name="password"
-                        icon='lock'
-                        iconPosition='left'
-                        placeholder='Password'
-                        type='password'
-                      />
-          
-                      <Button color='teal' fluid size='large'>
-                        Login
-                      </Button>
-                    </Segment>
-                  </Form>
-                </Grid.Column>
-              </Grid>
-            </div>
-          </div>
-        </Grid.Column>
-        
-        <Grid.Column>
-          <Table celled textAlign='center' >
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>John</Table.Cell>
-                <Table.Cell>Approved</Table.Cell>
-                <Table.Cell>None</Table.Cell>
-                <Table.Cell>adasd</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
-        </Grid.Column>
-
-      </Grid>
 */
